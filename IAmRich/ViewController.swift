bdb0a712cab5aca65ac05804e7d6ecd3a7f304bf
Dilmurod Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2020-01-23 20:07:47 +0500

    first commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Dilmurod Ubaydullaev on 1/23/20.
+//  Copyright © 2020 Dilmurod Ubaydullaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
